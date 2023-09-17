e96bcd79bb37665fad3957c71297d01e671f3b3b
Your Name <you@example.com>
2023-09-17 11:35:46 -0400

    aider: Added Realtor.py
    # Aider chat conversation:
    
    USER: Using the properties/v3/list api on https://rapidapi.com/apidojo/api/realtor, build a script to look for a property using specific criteria.
    
    ASSISTANT: Sure, I can help with that. However, I need more specific details about the criteria you want to use to search for a property. For example, are you looking for properties in a specific location, within a certain price range, or with a certain number of bedrooms or bathrooms? Please provide as much detail as possible.
    
    USER: use fields for user to provide an area (aceptable options should be city, zip, county), a min and max price, a minimum number of bedrooms, a minimum number of bathrooms, a minimum number of car garage
    
    ASSISTANT: Alright, we will need to create a Python script that interacts with the Realtor API. We will use the requests library to make HTTP requests to the API, and the json library to handle the JSON responses from the API. We will also need to use the getpass library to securely handle the API key.
    
    Here's a step-by-step breakdown of what we need to do:
    
    1. Import the necessary libraries.
    2. Define a function to get user input for the search criteria.
    3. Define a function to build the API request based on the user's search criteria.
    4. Define a function to make the API request and handle the response.
    5. Call these functions in the main part of the script.
    
    Here's how we can modify the Realtor.py file to implement this:
    
    ```python
    Realtor.py
    <<<<<<< HEAD
    
    =======
    import getpass
    import requests
    import json
    
    def get_search_criteria():
        area = input("Enter the area (city, zip, county): ")
        min_price = input("Enter the minimum price: ")
        max_price = input("Enter the maximum price: ")
        min_bedrooms = input("Enter the minimum number of bedrooms: ")
        min_bathrooms = input("Enter the minimum number of bathrooms: ")
        min_garage = input("Enter the minimum number of car garage: ")
    
        return area, min_price, max_price, min_bedrooms, min_bathrooms, min_garage
    
    def build_request(area, min_price, max_price, min_bedrooms, min_bathrooms, min_garage):
        url = "https://realtor.p.rapidapi.com/properties/v3/list"
        querystring = {
            "sort":"relevance",
            "city":area,
            "limit":"200",
            "offset":"0",
            "state_code":"WA",
            "price_min":min_price,
            "price_max":max_price,
            "beds_min":min_bedrooms,
            "baths_min":min_bathrooms,
            "garage":min_garage
        }
        headers = {
            'x-rapidapi-host': "realtor.p.rapidapi.com",
            'x-rapidapi-key': getpass.getpass("Enter your RapidAPI key: ")
        }
    
        return url, headers, querystring
    
    def make_request(url, headers, querystring):
        response = requests.request("GET", url, headers=headers, params=querystring)
        if response.status_code == 200:
            return json.loads(response.text)
        else:
            return None
    
    def main():
        area, min_price, max_price, min_bedrooms, min_bathrooms, min_garage = get_search_criteria()
        url, headers, querystring = build_request(area, min_price, max_price, min_bedrooms, min_bathrooms, min_garage)
        response = make_request(url, headers, querystring)
        if response is not None:
            print(json.dumps(response, indent=4))
        else:
            print("Failed to get response from API.")
    
    if __name__ == "__main__":
        main()
    >>>>>>> updated
    ```
    
    Please replace `"state_code":"WA"` with the appropriate state code for your search. Also, note that the user will be prompted to enter their RapidAPI key each time they run the script. This is to avoid storing the API key in the script, which would be a security risk.

---
+++ Realtor.py
@@ -0,0 +1,53 @@
+import getpass
+import requests
+import json
+
+def get_search_criteria():
+    area = input("Enter the area (city, zip, county): ")
+    min_price = input("Enter the minimum price: ")
+    max_price = input("Enter the maximum price: ")
+    min_bedrooms = input("Enter the minimum number of bedrooms: ")
+    min_bathrooms = input("Enter the minimum number of bathrooms: ")
+    min_garage = input("Enter the minimum number of car garage: ")
+    
+    return area, min_price, max_price, min_bedrooms, min_bathrooms, min_garage
+
+def build_request(area, min_price, max_price, min_bedrooms, min_bathrooms, min_garage):
+    url = "https://realtor.p.rapidapi.com/properties/v3/list"
+    querystring = {
+        "sort":"relevance",
+        "city":area,
+        "limit":"200",
+        "offset":"0",
+        "state_code":"WA",
+        "price_min":min_price,
+        "price_max":max_price,
+        "beds_min":min_bedrooms,
+        "baths_min":min_bathrooms,
+        "garage":min_garage
+    }
+    headers = {
+        'x-rapidapi-host': "realtor.p.rapidapi.com",
+        'x-rapidapi-key': getpass.getpass("Enter your RapidAPI key: ")
+    }
+    
+    return url, headers, querystring
+
+def make_request(url, headers, querystring):
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    if response.status_code == 200:
+        return json.loads(response.text)
+    else:
+        return None
+
+def main():
+    area, min_price, max_price, min_bedrooms, min_bathrooms, min_garage = get_search_criteria()
+    url, headers, querystring = build_request(area, min_price, max_price, min_bedrooms, min_bathrooms, min_garage)
+    response = make_request(url, headers, querystring)
+    if response is not None:
+        print(json.dumps(response, indent=4))
+    else:
+        print("Failed to get response from API.")
+
+if __name__ == "__main__":
+    main()
