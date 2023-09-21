062a81e6b4f7f15118646b37ce3e26492d7e4c19
Your Name <you@example.com>
2023-09-20 21:42:01 -0400

    aider: Added file uploader at the beginning of the Streamlit app to ask the user to upload the file they want to work with before reading it and executing the rest of the program.
    # Aider chat conversation:
    
    USER: modify the code to ask the user to upload the file they want to work with before reading it and executing the rest of the program
    
    ASSISTANT: To implement this change, we need to modify the `fx.py` file. We will add a file uploader at the beginning of the Streamlit app, and then read the uploaded file using pandas.
    
    Here is the step-by-step explanation of the changes:
    
    1. Add a file uploader using `st.file_uploader` at the beginning of the Streamlit app.
    2. Read the uploaded file using pandas `pd.read_excel` function.
    3. The rest of the code remains the same.
    
    Here is the *edit block* for the changes:
    
    ```python
    fx.py
    <<<<<<< HEAD
    import streamlit as st
    import pandas as pd
    
    # Read the Excel file
    df = pd.read_excel(r'C:\Users\tvoil\OneDrive\Desktop\Aider\FX Model.xlsx', sheet_name='FX Rate Forecasts')
    
    # Function to filter data
    def filter_data(df, region, currency, strategy):
        if region:
            df = df[df['Region'].str.lower().isin([r.lower() for r in region])]
        if currency:
            df = df[df['Currency'].str.lower().isin([c.lower() for c in currency])]
        if strategy:
            df = df[df['FY24 Strategy'].str.lower().isin([s.lower() for s in strategy])]
        return df
    
    # Streamlit web app
    st.title('FX Rate Forecasts')
    
    # User input for search functionality
    region = st.multiselect('Search by Region', options=list(df['Region'].unique()))
    currency = st.multiselect('Search by Currency', options=list(df['Currency'].unique()))
    strategy = st.multiselect('Search by Strategy', options=list(df['FY24 Strategy'].unique()))
    
    # Filter data based on user input
    filtered_data = filter_data(df, region, currency, strategy)
    
    # Display data
    st.dataframe(filtered_data[['Region', 'Currency', 'FY24 Strategy', 'FY24 Forecast', 'FY24 Target', 'Spot', 'Forwards']])
    =======
    import streamlit as st
    import pandas as pd
    
    # Streamlit web app
    st.title('FX Rate Forecasts')
    
    # File uploader
    uploaded_file = st.file_uploader("Choose an Excel file", type='xlsx')
    
    if uploaded_file is not None:
        # Read the Excel file
        df = pd.read_excel(uploaded_file, sheet_name='FX Rate Forecasts')
    
        # Function to filter data
        def filter_data(df, region, currency, strategy):
            if region:
                df = df[df['Region'].str.lower().isin([r.lower() for r in region])]
            if currency:
                df = df[df['Currency'].str.lower().isin([c.lower() for c in currency])]
            if strategy:
                df = df[df['FY24 Strategy'].str.lower().isin([s.lower() for s in strategy])]
            return df
    
        # User input for search functionality
        region = st.multiselect('Search by Region', options=list(df['Region'].unique()))
        currency = st.multiselect('Search by Currency', options=list(df['Currency'].unique()))
        strategy = st.multiselect('Search by Strategy', options=list(df['FY24 Strategy'].unique()))
    
        # Filter data based on user input
        filtered_data = filter_data(df, region, currency, strategy)
    
        # Display data
        st.dataframe(filtered_data[['Region', 'Currency', 'FY24 Strategy', 'FY24 Forecast', 'FY24 Target', 'Spot', 'Forwards']])
    else:
        st.write("Please upload an Excel file.")
    >>>>>>> updated
    ```

--- fx.py
+++ fx.py
@@ -1,29 +1,35 @@
 import streamlit as st
 import pandas as pd
 
-# Read the Excel file
-df = pd.read_excel(r'C:\Users\tvoil\OneDrive\Desktop\Aider\FX Model.xlsx', sheet_name='FX Rate Forecasts')
-
-# Function to filter data
-def filter_data(df, region, currency, strategy):
-    if region:
-        df = df[df['Region'].str.lower().isin([r.lower() for r in region])]
-    if currency:
-        df = df[df['Currency'].str.lower().isin([c.lower() for c in currency])]
-    if strategy:
-        df = df[df['FY24 Strategy'].str.lower().isin([s.lower() for s in strategy])]
-    return df
-
 # Streamlit web app
 st.title('FX Rate Forecasts')
 
-# User input for search functionality
-region = st.multiselect('Search by Region', options=list(df['Region'].unique()))
-currency = st.multiselect('Search by Currency', options=list(df['Currency'].unique()))
-strategy = st.multiselect('Search by Strategy', options=list(df['FY24 Strategy'].unique()))
+# File uploader
+uploaded_file = st.file_uploader("Choose an Excel file", type='xlsx')
 
-# Filter data based on user input
-filtered_data = filter_data(df, region, currency, strategy)
+if uploaded_file is not None:
+    # Read the Excel file
+    df = pd.read_excel(uploaded_file, sheet_name='FX Rate Forecasts')
 
-# Display data
-st.dataframe(filtered_data[['Region', 'Currency', 'FY24 Strategy', 'FY24 Forecast', 'FY24 Target', 'Spot', 'Forwards']])
+    # Function to filter data
+    def filter_data(df, region, currency, strategy):
+        if region:
+            df = df[df['Region'].str.lower().isin([r.lower() for r in region])]
+        if currency:
+            df = df[df['Currency'].str.lower().isin([c.lower() for c in currency])]
+        if strategy:
+            df = df[df['FY24 Strategy'].str.lower().isin([s.lower() for s in strategy])]
+        return df
+
+    # User input for search functionality
+    region = st.multiselect('Search by Region', options=list(df['Region'].unique()))
+    currency = st.multiselect('Search by Currency', options=list(df['Currency'].unique()))
+    strategy = st.multiselect('Search by Strategy', options=list(df['FY24 Strategy'].unique()))
+
+    # Filter data based on user input
+    filtered_data = filter_data(df, region, currency, strategy)
+
+    # Display data
+    st.dataframe(filtered_data[['Region', 'Currency', 'FY24 Strategy', 'FY24 Forecast', 'FY24 Target', 'Spot', 'Forwards']])
+else:
+    st.write("Please upload an Excel file.")
